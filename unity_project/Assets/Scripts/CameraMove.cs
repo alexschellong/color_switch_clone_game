571bcd7a480bb3828d7ccf1c1024afdba7d58e46
alexschellong <alex.schellong@gmail.com>
2024-08-30 01:19:43 +0300

    Camera movement bug fix

--- unity_project/Assets/Scripts/CameraMove.cs
+++ unity_project/Assets/Scripts/CameraMove.cs
@@ -8,6 +8,7 @@ public class CameraMove : MonoBehaviour
     public Transform ballPos;
 
     private bool ballPast45 = false;
+    private bool jumpInput = false;
 
     private float ballPosPresent;
     private float ballPosPrevious;
@@ -18,8 +19,13 @@ public class CameraMove : MonoBehaviour
     }
     private void Update()
     {
+        if (Input.anyKeyDown)
+        {
+            jumpInput = true;
+        }
+
         //If the ball is above 45% of the screen attach the camera to it. Once it starts falling deattach it.
-        if (!ballPast45)
+        if (!ballPast45 && jumpInput)
         {
             Vector2 viewPos = mainCamera.WorldToViewportPoint(ballPos.position);
 
@@ -27,26 +33,22 @@ public class CameraMove : MonoBehaviour
             {
                 this.transform.parent = ballPos;
                 ballPast45 = true;
-
+                jumpInput = false;
                 ballPosPrevious = ballPos.position.y;
             }
         }
         else
         {
             ballPosPresent = ballPos.position.y;
-            //-2 is arbitrary. Using 0 sometimes detaches the camera while the ball is not below 45% of the screen
-            if (ballPosPresent - ballPosPrevious < -2)
+            //ball is falling
+            if (ballPosPresent - ballPosPrevious < 0)
             {
-                //ball is falling
                 Vector2 viewPos = mainCamera.WorldToViewportPoint(ballPos.position);
                 ballPosPrevious = ballPosPresent;
                 this.transform.parent = null;
 
-                //check if the ball is below 45% screen height to avoid reparenting the camera immediately
-                if (viewPos.y < 0.45f)
-                {
-                    ballPast45 = false;
-                }
+                ballPast45 = false;
+                
             }
             else
             {
